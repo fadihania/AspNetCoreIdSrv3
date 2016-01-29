a261a29ca663730dd5063a188601b942d7b46a30
Fadi Hania <fhania@gmail.com>
2016-01-29 20:44:12 +0200

    Add in memory users, clients and scopes

---
+++ AspNetCoreIdSrv3/src/AspNetCoreIdSrv3/Configuration/Clients.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using IdentityServer3.Core.Models;
+
+namespace AspNetCoreIdSrv3.Configuration
+{
+    public class Clients
+    {
+        public static List<Client> Get()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientName = "Test Client",
+                    ClientId = "test",
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // server to server communication
+                    Flow = Flows.ResourceOwner,
+
+                    // only allowed to access api1
+                    AllowedScopes = new List<string>
+                    {
+                        "openid",
+                        "email",
+                        "profile",
+                        "api1"
+                    }
+                }
+            };
+        }
+    }
+}
