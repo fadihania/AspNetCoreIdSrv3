90c4e6e08e9c748e7d497a0466ac4cab367e7d4f
Fadi Hania <fhania@gmail.com>
2016-01-29 20:53:01 +0200

    Add startup configuration and protect API controller with Authorize attribute

--- AspNetCoreIdSrv3/src/AspNetCoreIdSrv3/Controllers/ValuesController.cs
+++ AspNetCoreIdSrv3/src/AspNetCoreIdSrv3/Controllers/ValuesController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authorization;
 
 namespace AspNetCoreIdSrv3.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
--- AspNetCoreIdSrv3/src/AspNetCoreIdSrv3/Startup.cs
+++ AspNetCoreIdSrv3/src/AspNetCoreIdSrv3/Startup.cs
@@ -7,6 +7,11 @@ using Microsoft.AspNet.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using IdentityServer3.Core.Configuration;
+using AspNetCoreIdSrv3.Configuration;
+using System.Security.Cryptography.X509Certificates;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace AspNetCoreIdSrv3
 {
@@ -26,17 +31,44 @@ namespace AspNetCoreIdSrv3
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDataProtection();
             // Add framework services.
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IApplicationEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
             app.UseIISPlatformHandler();
+
+            app.Map("/auth", authApp =>
+            {
+                var certFile = env.ApplicationBasePath + "\\idsrv3test.pfx";
+                var idsrvOptions = new IdentityServerOptions
+                {
+                    Factory = new IdentityServerServiceFactory()
+                                    .UseInMemoryUsers(Users.Get())
+                                    .UseInMemoryClients(Clients.Get())
+                                    .UseInMemoryScopes(Scopes.Get()),
+                    SigningCertificate = new X509Certificate2(certFile, "idsrv3test"),
+                    RequireSsl = false
+                };
+                authApp.UseIdentityServer(idsrvOptions);
+            });
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap = new Dictionary<string, string>();
+
+            app.UseJwtBearerAuthentication(options =>
+            {
+                options.Authority = "http://localhost:5000/auth";
+                options.RequireHttpsMetadata = false;
+
+                options.Audience = "http://localhost:5000/auth/resources";
+                options.AutomaticAuthenticate = true;
+            });
 
             app.UseStaticFiles();
 
